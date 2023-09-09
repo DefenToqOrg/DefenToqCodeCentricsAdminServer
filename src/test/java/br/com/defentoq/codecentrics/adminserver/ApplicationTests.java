63477fac93866405d9794d651c5b76ebf3cf0f0e
Alex <alex.valentim.goncalves@outlook.com>
2023-09-09 10:45:24 -0300

    commit inicial

---
+++ src/test/java/br/com/defentoq/codecentrics/adminserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.defentoq.codecentrics.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
